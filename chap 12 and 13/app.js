ea0a7db0a6f6c156ac3e5a2e12780b095de2776d
Shahbaz Suirya <155903516+shaib000@users.noreply.github.com>
2024-03-16 02:07:48 +0500

    Add files via upload

---
+++ chap 12 and 13/app.js
@@ -0,0 +1,98 @@
+// var input = prompt("Enter a character:");
+
+// if (input >= '0' && input <= '9') {
+//     console.log("The input is a number.");
+// } else if (input >= 'A' && input <= 'Z') {
+//     console.log("The input is an uppercase letter.");
+// } else if (input >= 'a' && input <= 'z') {
+//     console.log("The input is a lowercase letter.");
+// } else {
+//     console.log("Invalid input.");
+// }
+
+
+
+// var num1 = prompt("Enter the first integer:");
+// var num2 = prompt("Enter the second integer:");
+
+// if (num1 > num2) {
+//     console.log("The larger integer is: " + num1);
+// } else if (num2 > num1) {
+//     console.log("The larger integer is: " + num2);
+// } else {
+//     console.log("Both integers are equal.");
+// }
+ 
+
+
+
+
+// var number = prompt("Enter a number:");
+
+// if (number > 0) {
+//     console.log("The number is positive.");
+// } else if (number < 0) {
+//     console.log("The number is negative.");
+// } else {
+//     console.log("The number is zero.");
+// }
+
+
+
+
+// var character = prompt("Enter a character:");
+
+// var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// var isVowel = false;
+
+// if (character.length === 1) {
+//     for (var i = 0; i < vowels.length; i++) {
+//         if (character.toLowerCase() === vowels[i]) {
+//             isVowel = true;
+//             break;
+//         }
+//     }
+// }
+
+// console.log(isVowel);
+
+
+
+// var correctPassword = "shahbaz";
+
+// var userPassword = prompt("Enter your password:");
+
+// if (!userPassword) {
+//     console.log("Please enter your password.");
+// } else if (userPassword === correctPassword) {
+//     console.log("Correct! The password you entered matches the original password.");
+// } else {
+//     console.log("Incorrect password.");
+// }
+
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+
+
+// var time = prompt("Enter the time in 24-hour clock format (e.g., 1900 for 7pm):");
+
+// if (time >= '0000' && time < '1200') {
+//     console.log("Good morning!");
+// } else if (time >= '1200' && time < '1700') {
+//     console.log("Good afternoon!");
+// } else if (time >= '1700' && time < '2100') {
+//     console.log("Good evening!");
+// } else if (time >= '2100' && time <= '2359') {
+//     console.log("Good night!");
+// } else {
+//     console.log("Invalid time format.");
+// }
---
+++ code 10-11/app.js
@@ -0,0 +1,211 @@
+// var city=prompt("enter city name")
+// alert("welcome to CITY OF LIGHTS")
+
+// var gender=prompt("enter your gender")
+// if(gender==male){
+// alert("wlcome sir")
+// }
+// else if(gender==female){
+//     alert("wlecome maam")
+// }
+
+
+// var traffic =prompt("enter colour of traffic signal")
+
+// if(traffic=="red"){
+// console.log("must stop")
+
+// } else if(traffic=="green"){
+//     console.log("go")
+// }else if (traffic=="yellow"){
+//     console.log("ready to move")
+// }
+
+
+// var fuel=prompt("enter fuel")
+// if (fuel <0.25){
+//     console.log("Please refill the fuel in your car")
+// }
+
+
+// ------------------------------------------------------
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// output=true
+
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// output no display
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// 2 and 4 is true
+
+
+
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// output :- cost equals
+
+
+
+
+
+
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+
+//     output true
+
+
+
+
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+//     output car is smaler then cat
+
+
+// ----------------------------------------------------
+
+
+
+
+// const subject1Marks = parseInt(prompt("Enter marks obtained in subject 1:"));
+// const subject2Marks = parseInt(prompt("Enter marks obtained in subject 2:"));
+// const subject3Marks = parseInt(prompt("Enter marks obtained in subject 3:"));
+// const totalMarks = parseInt(prompt("Enter total marks:"));
+
+// const totalObtainedMarks = subject1Marks + subject2Marks + subject3Marks;
+// const percentage = (totalObtainedMarks / totalMarks) * 100;
+
+// let grade;
+// if (percentage >= 90) {
+//     grade = 'A';
+// } else if (percentage >= 80) {
+//     grade = 'B';
+// } else if (percentage >= 70) {
+//     grade = 'C';
+// } else if (percentage >= 60) {
+//     grade = 'D';
+// } else {
+//     grade = 'F';
+// }
+
+// console.log("Percentage: " + percentage.toFixed(2) + "%");
+// console.log("Grade: " + grade);
+// --------------------------------------------------------------------
+
+
+// var sc=9
+
+// var ac=prompt("enter number from 1 to 10")
+
+// if(ac==sc){
+//     console.log("Bingo! Correct answer")
+// }else if(ac==10 || ac==8  ){
+//     console.log("close enough")
+// }else if(ac<=7){
+//     console.log("wrong")
+// }
+
+
+
+// const number = parseInt(prompt("Enter a number:"));
+
+// if (number % 3 === 0) {
+//     console.log(number + " is divisible by 3.");
+// } else {
+//     console.log(number + " is not divisible by 3.");
+// }
+
+
+
+
+// const number = parseInt(prompt("Enter a number:"));
+
+// if (number % 2 === 0) {
+//     console.log(number + " is an even number.");
+// } else {
+//     console.log(number + " is an odd number.");
+// }
+
+
+
+// const temperature = parseInt(prompt("Enter the temperature:"));
+
+// if (temperature > 40) {
+//     console.log("It is too hot outside.");
+// } else if (temperature > 30) {
+//     console.log("The Weather today is Normal.");
+// } else if (temperature > 20) {
+//     console.log("Today’s Weather is cool.");
+// } else if (temperature > 10) {
+//     console.log("OMG! Today’s weather is so Cool.");
+// } else {
+//     console.log("Temperature is below 10 degrees.");
+// }
+
+
+// const firstNumber = parseFloat(prompt("Enter the first number:"));
+// const secondNumber = parseFloat(prompt("Enter the second number:"));
+// const operation = prompt("Enter the operation (+, -, *, /, %):");
+
+// let result;
+
+// if (operation === '+') {
+//     result = firstNumber + secondNumber;
+// } else if (operation === '-') {
+//     result = firstNumber - secondNumber;
+// } else if (operation === '*') {
+//     result = firstNumber * secondNumber;
+// } else if (operation === '/') {
+//     if (secondNumber === 0) {
+//         console.log("Error: Cannot divide by zero.");
+//     } else {
+//         result = firstNumber / secondNumber;
+//     }
+// } else if (operation === '%') {
+//     result = firstNumber % secondNumber;
+// } else {
+//     console.log("Invalid operation.");
+// }
+
+// if (operation !== '/' && operation !== '%' && result !== undefined) {
+//     console.log("Result:", result);
+// }
+
